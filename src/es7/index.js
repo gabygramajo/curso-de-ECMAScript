0d66f884e848dbf6d9f3d494e6a7c8633ac6a65d
gaby <ggabrieldeveloper@gmail.com>
2022-07-19 11:18:30 -0300

    adding class 5

---
+++ src/es7/index.js
@@ -0,0 +1,16 @@
+// ¿Qué se implementó en ES7?
+
+// metodo include()
+let numbers = [ 1, 2, 3, 7, 8];
+if (numbers.includes(7)) {
+  console.log('Si se encuentra el valor 7');
+} else {
+  console.log('No se encuentra.');
+}
+
+// Operador aritmético de potenciación: ** 
+
+let base = 2;
+let exponent = 3;
+let result = base ** exponent;
+console.log(result);
