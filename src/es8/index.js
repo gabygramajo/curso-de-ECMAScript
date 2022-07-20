2154dc76a2db0dfd8569170c02aa5588b57461f4
gaby <ggabrieldeveloper@gmail.com>
2022-07-20 20:53:42 -0300

    Async Await

--- src/es8/index.js
+++ src/es8/index.js
@@ -18,3 +18,30 @@ console.log(values);
 const string = 'hello';
 console.log(string.padStart(8, 'hi '));
 console.log(string.padEnd(13, ' -----'));
+
+// Async Await
+
+const helloWorld = () => {
+  return new Promise( ( resolve, reject )=> {
+    (false)
+    ? setTimeout(() => resolve('Hello World'), 3000)
+    : reject(new Error('Test Error'));
+  }); 
+}
+
+const helloAsync = async () => {
+  const hello = await helloWorld();
+  console.log(hello);
+}
+helloAsync();
+
+// Manera Correcta de ejecutar Async Await
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorld();
+    console.log(hello);
+  } catch (error) {
+    console.log(error);
+  }
+}
+anotherFunction();
