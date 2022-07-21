c1b13c75ccce2ead9078ea2d82e05a930ff8c750
gaby <ggabrieldeveloper@gmail.com>
2022-07-21 11:23:17 -0300

    adding es11 - part2

--- src/es11/index.js
+++ src/es11/index.js
@@ -7,3 +7,32 @@ button.addEventListener("click", async function () {
   const module = await import("./file.js");
   module.hello();
 })
+
+// bigInt
+const aBigNumber = 9007199254740991n; // op1
+const anotherBigNumber = BigInt(9007199254740991); // op2
+
+// Promise.AllSettled()
+
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve 1"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 2"));
+
+Promise.allSettled([promise1, promise2, promise3])
+  .then(response => console.log(response));
+
+// globalThis
+
+// operador logico ?? para null
+const fooo = null ?? 'default string';
+console.log(fooo);
+
+// opcional chaining ?.
+const user = {}
+console.log(user?.profile?.email);
+// ej 2
+if(user?.profile?.email){
+  console.log('email');
+} else {
+  console.log('fail');
+}
